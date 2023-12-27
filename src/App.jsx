0595a5019d4656664b9357a2226a0d669ccba9f5
Gregory Shogan <gregoryshogan.e@gmail.com>
2023-12-27 14:56:38 -0600

    git@github.com:gshogan2181/Block27.git

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
+
+
+
+ function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    console.log("New token: ", token);
+  }, [token]);
+
+  return (
+    <>
+  
+      <Authenticate token={token} setToken={setToken} />
+      <SignUpForm token={token} setToken={setToken} />
+
+      
+    </>
+  );
+}
+
+
+export default App;
